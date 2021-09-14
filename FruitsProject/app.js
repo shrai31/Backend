69eb655facc540c991ee9a0930baa44aff4f5a86
Himanshu <shrai31@gmail.com>
2021-09-14 08:28:17 +0530

    Update

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -20,8 +20,64 @@ const fruit = new Fruit({
     review:"Pretty solid as a fruit."
 });
 
-fruit.save();
+// fruit.save();
 
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+})
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+    name: "John",
+    age: 37
+})
+
+person.save();
+
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 10,
+    review: "Energy is High"
+})
+
+const banana = new Fruit({
+    name: "Banana",
+    score: 8,
+    review: "Red in color"
+})
+
+const orange = new Fruit({
+    name: "Orange",
+    score: 2,
+    review: "too much sour"
+})
+
+// Fruit.insertMany([kiwi, orange, banana], function(err){
+//     if(err){
+//         console.log(err);
+//     }
+//     else {
+//         console.log("Succesfully saved all the fruits to fruitsDB");
+//     }
+// })
+
+Fruit.find(function(err, fruits){
+    if(err){
+        console.log(err);
+    }
+    else{
+        // fruits.map( items =>{
+        //     console.log(items.name);
+        // })
+        // for(let i in fruits)
+        // console.log(fruits[i].name);
+            fruits.forEach(item=>{
+                console.log(item.name);
+            })
+    }
+});
 const findDocuments = function(db, callback){
     const collection = db.collection('fruits');
 
